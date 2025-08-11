2ba383963b0e6ad4e619218d6b887696839b54fb
Frederik Kristensen <frede4947@gmail.com>
2025-08-11 15:59:29 +0200

    Setup for Tailwind and components keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
 import { useState } from 'react';
-import './style.css';
+import Keys from './components/Keys.tsx';
 
 function App() {
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+    <div className='bg-sky-900 min-h-screen'>
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand"></div>
+          <div className="current-operand"></div>
+        </div>
+        
+        <Keys/>
+
       </div>
-      
-      <button className="span-two">CE</button>
-      <button>C</button>
-      <button>/</button>
-      
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>*</button>
-
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>-</button>
-
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>+</button>
-
-      <button>0</button>
-      <button>.</button>
-      <button className="span-two">=</button>
     </div>
   );
 }
---
+++ src/components/Keys.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Keys = ( ) => {
+  return (
+    <div className='text-white'>
+      <i>CE</i>
+      <i>C</i>
+      <i>/</i>
+
+      <i>7</i>
+      <i>8</i>
+      <i>9</i>
+
+      <i>4</i>
+      <i>5</i>
+      <i>6</i>
+      <i>-</i>
+      
+      <i>1</i>
+      <i>2</i>
+      <i>3</i>  
+      <i>+</i>
+
+      <i>0</i>
+      <i>.</i>
+      <i>=</i>
+    </div>
+  );
+}
+
+export default Keys
